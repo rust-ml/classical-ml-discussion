39b73797b4301f695c5b4d3914a5441acbaf848e
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-05-19 16:38:32 +0100

    Skeleton of Fit and IncrementalFit implementation

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,9 +3,13 @@ extern crate ndarray;
 #[macro_use]
 extern crate derive_more;
 
-use linfa::{Transformer, Blueprint};
+use linfa::{Blueprint, Fit, IncrementalFit, Transformer};
 use ndarray::{Array1, ArrayBase, Data, Ix1};
 use std::error::Error;
+
+/// Short-hand notations
+type Input<S: Data<Elem = f64>> = ArrayBase<S, Ix1>;
+type Output = Array1<f64>;
 
 /// Fast-and-dirty error struct
 #[derive(Debug, Eq, PartialEq, From, Display)]
@@ -28,14 +32,46 @@ pub struct StandardScaler {
 /// It keeps track of the number of samples seen so far, to allow for
 /// incremental computation of mean and standard deviation.
 pub struct OnlineOptimizer {
-    n_samples: u64
+    n_samples: u64,
+}
+
+impl<S> Fit<Config, Input<S>, Output> for OnlineOptimizer
+where
+    S: Data<Elem = f64>,
+{
+    type Error = ScalingError;
+
+    fn fit(
+        &self,
+        inputs: &Input<S>,
+        targets: &Output,
+        blueprint: Config,
+    ) -> Result<StandardScaler, Self::Error> {
+        unimplemented!()
+    }
+}
+
+impl<S> IncrementalFit<StandardScaler, Input<S>, Output> for OnlineOptimizer
+where
+    S: Data<Elem = f64>,
+{
+    type Error = ScalingError;
+
+    fn incremental_fit(
+        &self,
+        inputs: &Input<S>,
+        targets: &Output,
+        transformer: StandardScaler,
+    ) -> Result<StandardScaler, Self::Error> {
+        unimplemented!()
+    }
 }
 
 pub struct Config {
     // Delta degrees of freedom.
     // With ddof = 1, you get the sample standard deviation
     // With ddof = 0, you get the population standard deviation
-    ddof: u8
+    ddof: u8,
 }
 
 /// Defaults to computing the sample standard deviation.
@@ -45,13 +81,20 @@ impl Default for Config {
     }
 }
 
-impl<S> Transformer<ArrayBase<S, Ix1>, Array1<f64>> for StandardScaler
+impl<S> Blueprint<Input<S>, Output> for Config
+where
+    S: Data<Elem = f64>,
+{
+    type Transformer = StandardScaler;
+}
+
+impl<S> Transformer<Input<S>, Output> for StandardScaler
 where
     S: Data<Elem = f64>,
 {
     type Error = ScalingError;
 
-    fn transform(&self, inputs: &ArrayBase<S, Ix1>) -> Result<Array1<f64>, Self::Error>
+    fn transform(&self, inputs: &Input<S>) -> Result<Output, Self::Error>
     where
         S: Data<Elem = f64>,
     {
