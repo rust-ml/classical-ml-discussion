6aa09e09ae6ddb9227342be90373168f1de2a211
LukeMathWalker <lucapalmieri1993@hotmail.com>
2019-05-19 16:49:34 +0100

    Implemented Fit trait

--- examples/basic.rs
+++ examples/basic.rs
@@ -4,7 +4,7 @@ extern crate ndarray;
 extern crate derive_more;
 
 use linfa::{Blueprint, Fit, IncrementalFit, Transformer};
-use ndarray::{Array1, ArrayBase, Data, Ix1};
+use ndarray::{Array1, ArrayBase, Data, Ix1, Axis};
 use std::error::Error;
 
 /// Short-hand notations
@@ -24,9 +24,9 @@ pub struct StandardScaler {
     // Delta degrees of freedom.
     // With ddof = 1, you get the sample standard deviation
     // With ddof = 0, you get the population standard deviation
-    ddof: u8,
-    mean: f64,
-    standard_deviation: f64,
+    pub ddof: u8,
+    pub mean: f64,
+    pub standard_deviation: f64,
 }
 
 /// It keeps track of the number of samples seen so far, to allow for
@@ -44,10 +44,19 @@ where
     fn fit(
         &self,
         inputs: &Input<S>,
-        targets: &Output,
+        _targets: &Output,
         blueprint: Config,
     ) -> Result<StandardScaler, Self::Error> {
-        unimplemented!()
+        if inputs.len() == 0 {
+            return Err(ScalingError {})
+        }
+        let mean = inputs.mean_axis(Axis(0)).into_scalar();
+        let standard_deviation = inputs.std_axis(Axis(0), blueprint.ddof as f64).into_scalar();
+        Ok(StandardScaler {
+            ddof: blueprint.ddof,
+            mean,
+            standard_deviation
+        })
     }
 }
 
